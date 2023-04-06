aec7e915f291fd8552a61ee10b96c676018400c1
thekavikumar <kavikumarceo@gmail.com>
2023-04-06 11:45:38 +0530

    auth0 added

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -5,17 +5,16 @@ import TextSpan from "./TextSpan";
 function Login() {
   const sentence = "EDUCATE. EXPLORE. EVOLVE.".split("");
 
-  const { user, isAuthenticated, isLoading, logout, loginWithRedirect } =
-    useAuth0();
+  const { isLoading, loginWithRedirect } = useAuth0();
   if (isLoading) {
     return <div>Loading ...</div>;
   }
   return (
-    <div className="flex min-h-screen flex-col gap-3 items-center justify-center">
+    <div className="flex min-h-screen flex-col gap-3 items-center justify-center p-5 sm:p-8">
       <h1 className="font-extrabold text-gray-900 text-2xl  lg:text-5xl text-center">
         WELCOME TO EDUHUB
       </h1>
-      <h2 className="font-extrabold text-gray-900 text-3xl lg:text-6xl text-center">
+      <h2 className="font-extrabold text-gray-900 text-3xl lg:text-6xl text-center flex-wrap">
         {sentence.map((letter, index) => {
           return (
             <TextSpan key={index}>
@@ -25,7 +24,7 @@ function Login() {
         })}
       </h2>
       <button
-        className="p-3 w-44 text-[18px] font-bold mt-2 border-2 border-black rounded-[6px] bg-[#F9F9F9] hover:bg-black hover:text-[#ffffff] lg:text-[23px] lg:w-56"
+        className="p-3 w-44 text-[18px] font-bold mt-2 border-2 border-gray-900 transition-all duration-200 rounded-[6px] bg-[#F9F9F9] hover:bg-gray-900 hover:text-[#ffffff] lg:text-[23px] lg:w-56"
         onClick={() => loginWithRedirect()}
       >
         Log In
